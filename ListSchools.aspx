fc5909a7a577d06f765df503097b8f96d5513986
Chris <Christopher.Strolia-Davis@TheIngagePartners.com>
2011-10-22 14:33:50 -0400

    Modified to use new database connection. Also modified to use updated DB model.

--- ListSchools.aspx
+++ ListSchools.aspx
@@ -1,29 +1,21 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/COCAsite.master"  CodeFile="ListSchools.aspx.cs" Inherits="ListSchools" %>
-
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" Runat="Server">        
     <title>List Schools</title>
     <link type="text/css" href="CocaChild.css" rel="Stylesheet" />
-
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" Runat="Server">
     <div>
     
-<%--        <asp:GridView ID="gvSchoolList" runat="server" EnableModelValidation="True" 
+            <asp:GridView ID="gvSchoolList" runat="server" EnableModelValidation="True" 
             AutoGenerateColumns="False" >
             <Columns>
                 <asp:HyperLinkField DataTextField="Name" HeaderText="School Name" 
                     SortExpression="Name" />
-                <asp:BoundField DataField="City" HeaderText="City" />
-                <asp:BoundField DataField="StateName" HeaderText="State" />
-                <asp:BoundField DataField="Zip" HeaderText="Zip" />
-            </Columns>
-        </asp:GridView>--%>
-            <asp:GridView ID="gvSchoolList" runat="server" EnableModelValidation="True" 
-            AutoGenerateColumns="False" >
-            <Columns>
-                <asp:HyperLinkField DataTextField="SchoolName" HeaderText="School Name" 
-                    SortExpression="SchoolName" />
                 <asp:BoundField DataField="City" HeaderText="City" />
                 <asp:BoundField DataField="StateName" HeaderText="StateName" />
                 <asp:BoundField DataField="Zip" HeaderText="Zip" />
             </Columns>
         </asp:GridView>
     </div>
+</asp:Content>
  
--- ListSchools.aspx.cs
+++ ListSchools.aspx.cs
@@ -1,88 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 using DAL;
-using System.Data;
-using System.Data.Linq;
 
 public partial class ListSchools : System.Web.UI.Page
 {
-    DalOld SchoolView = new DalOld();
-
     protected void Page_Load(object sender, EventArgs e)
     {
-        using(CocaDataContext ctx = new CocaDataContext()){
-       
+        using (CocaDataContext ctx = new CocaDataContext())
+        {
+
             gvSchoolList.DataSource = (
-                    from School d in ctx.Schools 
-                    select new {
-                        Name = d.Name, 
-                        City = d.City, 
-                        StateName = d.State.Name, 
+                    from School d in ctx.Schools
+                    select new
+                    {
+                        Name = d.Name,
+                        City = d.City,
+                        StateName = d.State.Name,
                         Zip = d.Zip
                     }
                 ).ToList();
             gvSchoolList.DataBind();
         }
-        //var sv = SchoolView.SchoolListView().ToTable();
-       
- 
-        //gvSchoolList.DataSource = SchoolView.SchoolListView();
-        //gvSchoolList.DataBind();
-        //using(CocaDataContext ctx = new CocaDataContext()){
-            //var data = from d in ctx.Schools select d;
-
-            //var sortedWords =
-            //    from w in words
-            //    orderby w.Length
-            //    select w;, 
-
-            //var results = (from School d in ctx.Schools orderby d.Name select d);
-                //.Including(d => d.State); 
-
-
-            //gvSchoolList.DataSource = SchoolView.SchoolListView();
-           // gvSchoolList.DataSource = results.ToList();
-            //gvSchoolList.DataSource = SchoolsWithStates;
-            //gvSchoolList.DataSource = Schools;
-            //gvSchoolList.DataBind();
-        }
-       
-            //SchoolView.SchoolListView();
-
-    //public static IList<customer> CustomersWithOrders
-    //{
-    //    get
-    //    {
-    //        return Customers
-    //                .Select(c => new { Customer = c, Orders = c.Orders })
-    //        .ToList()
-    //                .Select(pair => pair.Customer)
-    //        .ToList();
-    //    }
-    //}
-    //public static IList<School> SchoolsWithStates
-    //{
-    //    get
-    //    {
-    //        return SchoolsWithStates
-    //                .Select(c => new { School = c, States = c.State.Name })
-    //        .ToList()
-    //                .Select(pair => pair.States)
-    //        .ToList();
-    //    }
-    //}
-
-    //public static IList<School> Schools
-    //{
-    //    get
-    //    {
-    //        return Schools  
-    //                .Select(c => new { School = c })
-    //        .ToList();
-    //    }
-    //}
+    }
 }
