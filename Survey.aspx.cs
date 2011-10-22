7561a16734385dd861dc1414e976c70c4b18709e
Kim.Painter <Kim.Painter@InsightBB.com>
2011-10-22 15:14:26 -0400

    Survey page modifications

--- Survey.aspx.cs
+++ Survey.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class NewSurvey : System.Web.UI.Page
+public partial class Survey : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
