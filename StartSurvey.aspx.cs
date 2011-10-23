3f3304e564061c684433c7b766c5cc0a38accc67
Kim.Painter <Kim.Painter@InsightBB.com>
2011-10-23 10:24:50 -0400

    StartSurvey.aspx verification against login - part 1 of 2

--- StartSurvey.aspx.cs
+++ StartSurvey.aspx.cs
@@ -4,11 +4,27 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using DAL;
 
 public partial class StartSurvey : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+    protected void btnStartSurvey_Click(object sender, EventArgs e)
+    {
+        using (CocaDataContext ctx = new CocaDataContext())
+        {
+            bool anonStudentExists = (from AnonStudent s in ctx.AnonStudents
+                                      where s.UserId == txtStudentID.Text && s.Password == txtPassword.Text
+                                       && s.StudentGroupSeasonId == Convert.ToInt32(txtARPID.Text)
+                                      select s).Any();
+
+            if (anonStudentExists)
+            {
+                //Session.Add() = txtStudentID.Text;
+            }
+        }
     }
 }
